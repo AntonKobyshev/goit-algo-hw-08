00b14301b94e211e8e7ba15cfd2e586e9b8bb586
Anton Kobyshev <antonkobyshev@gmail.com>
2024-01-31 22:42:30 +0100

    Update task.py

--- task.py
+++ task.py
@@ -11,22 +11,27 @@ class Cable:
 def connect_cables(cables):
     heap = cables[:]
     heapq.heapify(heap)
-    visualize_connections = ""
+    total_cost = 0
+    connections = []
 
     while len(heap) > 1:
         cable1 = heapq.heappop(heap)
         cable2 = heapq.heappop(heap)
 
         merged_cable = Cable(cable1.length + cable2.length, f"({cable1.color} > {cable2.color})")
-        visualize_connections += f"{cable1.color} => {cable2.color} " if len(heap) >= 1 else ""
+        connections.append(merged_cable)
+        total_cost += merged_cable.length
 
         heapq.heappush(heap, merged_cable)
 
-    return heap[0].length, visualize_connections
+    return total_cost, connections
 
 if __name__ == "__main__":
     cables = [Cable(3, "Red"), Cable(10, "Blue"), Cable(2, "Green"), Cable(14, "Yellow"), Cable(12, "Orange")]
-    result_heap, result_visualize = connect_cables(cables)
+    result_cost, result_connections = connect_cables(cables)
 
-    print(f"Порядок об'єднання: {result_visualize}")
-    print(f"Загальні витрати: {result_heap}")
+    print("З'єднання кабелів:")
+    for connection in result_connections:
+        print(f"{connection.color} - {connection.length}")
+
+    print(f"Загальна вартість з'єднання: {result_cost}")
